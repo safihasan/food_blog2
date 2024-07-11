a23c7803d34d0db0b3e6fae2fb21b14a52d03eab
Safi Hasan <safihasan@gmail.com>
2024-07-11 14:07:03 +0000

    debug

--- food_fusion/settings.py
+++ food_fusion/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-safihasan-foodblog2-g1s68qppjyt.ws.codeinstitute-ide.net']
 '.herokuapp.com'
@@ -79,15 +79,17 @@ WSGI_APPLICATION = 'food_fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-safihasan-foodblog2-g1s68qppjyt.ws.codeinstitute-ide.net",
     "https://*.herokuapp.com"
