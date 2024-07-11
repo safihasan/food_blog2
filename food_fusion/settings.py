6c86de267ab5290546229c7c51f966bef2555ae3
Safi Hasan <safihasan@gmail.com>
2024-07-11 13:01:57 +0000

    migration

--- food_fusion/settings.py
+++ food_fusion/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-safihasan-foodblog2-g1s68qppjyt.ws.codeinstitute-ide.net']
 '.herokuapp.com'
