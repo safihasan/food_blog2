69787305f97fc3f66f09f66aac389e266e09165d
Safi Hasan <safihasan@gmail.com>
2024-07-11 08:49:23 +0000

    deploy

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -11,5 +14,5 @@ class Post(models.Model):
 content = models.TextField()
 created_on = models.DateTimeField(auto_now_add=True)
 status = models.IntegerField(choices=STATUS, default=0)
-STATUS = ((0, "Draft"), (1, "Published"))
+
 
