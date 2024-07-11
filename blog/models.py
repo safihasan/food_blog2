8adeeaf8e490b32f2a243e183e6d02a164b02515
Safi Hasan <safihasan@gmail.com>
2024-07-11 15:14:47 +0000

    Add user display in post model

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,11 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -27,10 +32,3 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-
-
-class Post(models.Model):
-    # …
-
-    class Meta:
-        ordering = ["-created_on"]
