95e4216427ae01fdb09bc976267d5e96a89c5899
Safi Hasan <safihasan@gmail.com>
2024-07-14 16:40:16 +0000

    tests

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,10 @@ from .models import Post, Comment
 from .forms import CommentForm
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
-        """
+    """
     Returns all published posts in :model:`blog.Post`
     and displays them in a page of six posts.
     **Context**
@@ -21,12 +23,13 @@ class PostList(generic.ListView):
 
     :template:`blog/index.html`
     """
-queryset = Post.objects.filter(status = 1)
-template_name = "blog/index.html"
-paginate_by = 6
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+
 
 def post_detail(request, slug):
-        """
+    """
     Display an individual :model:`blog.Post`.
 
     **Context**
@@ -44,35 +47,36 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
-queryset = Post.objects.filter(status=1)
-post = get_object_or_404(queryset, slug=slug)
-comments = post.comments.all().order_by("-created_on")
-comment_count = post.comments.filter(approved=True).count()
-
-if request.method == "POST":
-    comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
-        messages.add_message(
-        request, messages.SUCCESS,
-        'Comment submitted and awaiting approval'
-    )
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
 
     comment_form = CommentForm()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post,
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form,
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
         },
-    )    
+    )
+
+
 def comment_edit(request, slug, comment_id):
     """
     Display an individual comment for edit.
@@ -93,16 +97,19 @@ def comment_edit(request, slug, comment_id):
         comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
 
-    if comment_form.is_valid() and comment.author == request.user:
+        if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.approved = False
             comment.save()
             messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
-    else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug])) 
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
 def comment_delete(request, slug, comment_id):
     """
     Delete an individual comment.
@@ -122,6 +129,7 @@ def comment_delete(request, slug, comment_id):
         comment.delete()
         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
