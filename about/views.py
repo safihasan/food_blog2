ca8861a8d8d08bc88b653ff35e8a03f20f5ee263
Safi Hasan <safihasan@gmail.com>
2024-07-14 16:35:14 +0000

    tests

--- about/views.py
+++ about/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
-
 # Create your views here.
+
+
 def about_me(request):
-     """
+    """
     Renders the most recent information on the website author
     and allows user collaboration requests.
 
@@ -20,14 +21,15 @@ def about_me(request):
     **Template**
     :template:`about/about.html`
     """
-if request.method == "POST":
-    collaborate_form = CollaborateForm(data=request.POST)
-    if collaborate_form.is_valid():
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
-    """
-    Renders the About page
-    """
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
@@ -35,6 +37,5 @@ if request.method == "POST":
         request,
         "about/about.html",
         {"about": about,
-        "collaborate_form": collaborate_form
-        },
+         "collaborate_form": collaborate_form},
     )
