c7481b5721b482b1a6fdc278922785971148c132
Safi Hasan <safihasan@gmail.com>
2024-07-16 02:15:17 +0000

    image

--- food_fusion/settings.py
+++ food_fusion/settings.py
@@ -101,16 +101,16 @@ WSGI_APPLICATION = 'food_fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+#DATABASES = {
+#   #'default': {
+   #    'ENGINE': 'django.db.backends.sqlite3',
+   #    'NAME': BASE_DIR / 'db.sqlite3',
+   #}
+#}
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
+DATABASES = {
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 if 'test' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
