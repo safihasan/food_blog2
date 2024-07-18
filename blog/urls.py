7590a398a5ae0b5de00253aba584d1fd81a1601b
Safi Hasan <safihasan@gmail.com>
2024-07-18 11:19:06 +0000

    favicon

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
-    path('static/images/favicon/favicon.ico', serve_favicon, name='favicon'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
